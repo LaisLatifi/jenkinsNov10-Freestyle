c536e113e44edcae2a38a27c2b11c082accaff25
LaisLatifi <mudaser1992@gmail.com>
2023-11-10 08:18:32 -0800

    adding 1st commit

---
+++ freeStyleTest1.java
@@ -0,0 +1,10 @@
+public class freeStyleTest1{
+    public static void main (String[]args){
+System.out.print("Hello Java");
+        System.out.print("Today is the November 10th");
+    };
+
+
+}
+
+
